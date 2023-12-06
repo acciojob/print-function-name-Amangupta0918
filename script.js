319baa8329f2c6cee8aaa8611b9a7eb0284d1a62
Amangupta0918 <68322087+Amangupta0918@users.noreply.github.com>
2023-12-06 12:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-function functionName()
-	{
-		alert("functionName");
-	}
-functionName()
+function myFunction() {
+    console.log(arguments.callee.name);
+}
+
+myFunction();  // This will print "myFunction"
